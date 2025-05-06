56dce56866168646f9093e89bbc1051096bd5128
Izhak James <hein_htet_23@hotmail.com>
2025-05-06 17:01:37 +0800

    Implemented my own Max heap

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,22 @@
 import Tree.MyAVLTree;
+import Tree.MyMaxHeap;
 import Tree.MyTree;
 
 public class Main {
     public static void main(String[] args) {
-        MyAVLTree mytree = new MyAVLTree();
-
-
-
-
-        mytree.insert(16);
-        mytree.insert(19);
-        mytree.insert(18);
-
-        System.out.println(mytree);
-
+        MyMaxHeap heap = new MyMaxHeap(10);
+        heap.insert(15);
+        heap.insert(10);
+        heap.insert(3);
+        heap.insert(8);
+        heap.insert(12);
+        heap.insert(9);
+        heap.insert(4);
+        heap.insert(1);
+        heap.insert(24);
+        System.out.println(heap);
+        heap.remove();
+        System.out.println(heap);
 
     }
 }
---
+++ src/Tree/MyMaxHeap.java
@@ -0,0 +1,82 @@
+package Tree;
+
+import java.util.Arrays;
+
+public class MyMaxHeap {
+    private int[] items;
+    private int pointer;
+
+    public MyMaxHeap(int size) {
+        items = new int[size];
+    }
+
+    public boolean isEmpty() {
+        return pointer == 0;
+    }
+
+    public boolean isFull() {
+        return pointer == items.length;
+    }
+
+    public void insert(int item) {
+        if (isFull()) {
+            throw new IllegalStateException("Heap is full");
+        }
+        items[pointer++] = item;
+        bubbleUp();
+
+    }
+
+    private void bubbleUp() {
+        int currrentIndex = pointer - 1;
+        int parent = (currrentIndex - 1) / 2;
+        while (currrentIndex > 0 && items[currrentIndex] > items[parent]) {
+            swap(currrentIndex, parent);
+            currrentIndex = parent;
+            parent = (currrentIndex - 1) / 2;
+        }
+    }
+
+    public int remove() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Heap is empty");
+        }
+        int first = items[0];
+        swap(0, pointer - 1);
+        items[--pointer] = 0;
+        bubbleDown();
+        return first;
+
+    }
+
+    private void bubbleDown() {
+        int currentIndex = 0;
+        int leftChildIndex = 1;
+        int rightChildIndex = 2;
+        while (items[currentIndex] < items[leftChildIndex] || items[currentIndex] < items[rightChildIndex]) {
+            if (items[currentIndex] < items[leftChildIndex]) {
+                swap(currentIndex, leftChildIndex);
+                currentIndex = leftChildIndex;
+            } else {
+                swap(currentIndex, rightChildIndex);
+                currentIndex = rightChildIndex;
+            }
+            leftChildIndex = 2 * currentIndex + 1;
+            rightChildIndex = 2 * currentIndex + 2;
+            if (leftChildIndex >= pointer || rightChildIndex >= pointer) {
+                break;
+            }
+        }
+    }
+
+    private void swap(int index1, int index2) {
+        int temp = items[index1];
+        items[index1] = items[index2];
+        items[index2] = temp;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(items);
+    }
+}
