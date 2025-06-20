e89fd6b9e21ced088ce6925f58f4b0f045dc5618
Izhak James <hein_htet_23@hotmail.com>
2025-06-20 23:52:03 +0800

    Improve coin change problem

--- src/LeetCode/DP/CoinChange.java
+++ src/LeetCode/DP/CoinChange.java
@@ -42,4 +42,20 @@ public class CoinChange {
 
         return map.getOrDefault(amount,-1);
     }
+
+    // Faster
+    public int solution2Q322(int[] coins, int amount) {
+        int[] dp = new int[amount+1];
+        for (int i = 1; i < amount+1; i++) {
+            dp[i] = amount+1;
+        }
+        for (int i = 1; i < amount+1; i++) {
+            for (int coin : coins) {
+                if (i - coin >= 0) {
+                    dp[i] = Math.min(dp[i], dp[i-coin]+1);
+                }
+            }
+        }
+        return dp[amount] != amount+1 ? dp[amount] : -1;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,9 @@ import Linear.util.PairCounters;
 public class Main {
     public static void main(String[] args) {
         CoinChange solution = new CoinChange();
-        int[] arr = {2};
+        int[] arr = {1,2,5};
 
-        System.out.println(solution.solutionQ322(arr, 1));
+        System.out.println(solution.solution2Q322(arr, 11));
 
 
 
